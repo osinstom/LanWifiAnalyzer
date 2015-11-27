ab12b3c339b1fc31e0437292504b6bdf76c5b6ef
Tomek Osiński <tomasz.osinski@attr.com.pl>
2015-11-27 11:41:17 +0100

    first commit

---
+++ app/src/main/java/pl/edu/pw/lan/wifianalyzer/Menu.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.lan.wifianalyzer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Menu extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
